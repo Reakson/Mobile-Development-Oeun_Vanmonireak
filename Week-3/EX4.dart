6a0df994971cb56a5da61e5572ead6bd5be8a42f
reakson <v.monireak15@gmail.com>
2026-05-28 14:05:43 +0700

    Changes of Structure

--- Week-3/EX4.dart
+++ Week-3/EX4.dart
@@ -1,233 +1,233 @@
 // Exersice 4
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 200,
-            height: 400,
-            color: Colors.white,
-            child: Column(
-              children: [
-                Container(
-                  color: Colors.orange[600],
-                  width: 200,
-                  height: 30,
-                  child: Center(
-                    child: const Text(
-                      "Welcome!",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 200,
+//             height: 400,
+//             color: Colors.white,
+//             child: Column(
+//               children: [
+//                 Container(
+//                   color: Colors.orange[600],
+//                   width: 200,
+//                   height: 30,
+//                   child: Center(
+//                     child: const Text(
+//                       "Welcome!",
+//                       style: TextStyle(color: Colors.black),
+//                     ),
+//                   ),
+//                 ),
 
-                Container(
-                  width: 180,
-                  height: 40,
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: const Text(
-                      "My Hobbies",
-                      style: TextStyle(color: Colors.white, fontSize: 10),
-                    ),
-                  ),
-                ),
+//                 Container(
+//                   width: 180,
+//                   height: 40,
+//                   margin: EdgeInsets.symmetric(vertical: 10),
+//                   decoration: BoxDecoration(
+//                     color: Colors.black,
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                   child: Center(
+//                     child: const Text(
+//                       "My Hobbies",
+//                       style: TextStyle(color: Colors.white, fontSize: 10),
+//                     ),
+//                   ),
+//                 ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Working Out",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     Container(
+//                       margin: EdgeInsets.symmetric(vertical: 5),
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Working Out",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
 
-                    Container(
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Running",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+//                     Container(
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Running",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Exploring",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     Container(
+//                       margin: EdgeInsets.symmetric(vertical: 5),
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Exploring",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
 
-                    Container(
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Cinema",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+//                     Container(
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Cinema",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Swimming",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     Container(
+//                       margin: EdgeInsets.symmetric(vertical: 5),
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Swimming",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
 
-                    Container(
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Music",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+//                     Container(
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Music",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Reading",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     Container(
+//                       margin: EdgeInsets.symmetric(vertical: 5),
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Reading",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
 
-                    Container(
-                      width: 80,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Travelling",
-                          style: TextStyle(color: Colors.black, fontSize: 10),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+//                     Container(
+//                       width: 80,
+//                       height: 40,
+//                       decoration: BoxDecoration(
+//                         color: Colors.blue,
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
+//                       child: Center(
+//                         child: Text(
+//                           "Travelling",
+//                           style: TextStyle(color: Colors.black, fontSize: 10),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
 
-                Spacer(),
+//                 Spacer(),
 
-                Container(
-                  color: Colors.orange[600],
-                  width: 200,
-                  height: 30,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                      ),
+//                 Container(
+//                   color: Colors.orange[600],
+//                   width: 200,
+//                   height: 30,
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       Container(
+//                         width: 20,
+//                         height: 20,
+//                         decoration: BoxDecoration(
+//                           shape: BoxShape.circle,
+//                           color: Colors.white,
+//                         ),
+//                       ),
 
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                      ),
+//                       Container(
+//                         width: 20,
+//                         height: 20,
+//                         decoration: BoxDecoration(
+//                           shape: BoxShape.circle,
+//                           color: Colors.white,
+//                         ),
+//                       ),
 
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
+//                       Container(
+//                         width: 20,
+//                         height: 20,
+//                         decoration: BoxDecoration(
+//                           shape: BoxShape.circle,
+//                           color: Colors.white,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
--- Week-4/Challenge.dart
+++ Week-4/Challenge.dart
@@ -1,59 +1,59 @@
 // Challenge
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-        body: Center(
-          child: Container(
-            width: 340,
-            height: 850,
-            color: Colors.white,
-            child: Column(
-              children: [
-                SizedBox(height: 100),
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.grey,
+//         body: Center(
+//           child: Container(
+//             width: 340,
+//             height: 850,
+//             color: Colors.white,
+//             child: Column(
+//               children: [
+//                 SizedBox(height: 100),
 
-                Container(height: 400, color: Colors.blue[900]),
+//                 Container(height: 400, color: Colors.blue[900]),
 
-                SizedBox(
-                  height: 100,
-                  child: Row(
-                    children: [
-                      Expanded(child: Container(color: Colors.green)),
+//                 SizedBox(
+//                   height: 100,
+//                   child: Row(
+//                     children: [
+//                       Expanded(child: Container(color: Colors.green)),
 
-                      Container(color: Colors.pinkAccent, width: 100),
-                    ],
-                  ),
-                ),
+//                       Container(color: Colors.pinkAccent, width: 100),
+//                     ],
+//                   ),
+//                 ),
 
-                SizedBox(
-                  height: 100,
-                  child: Row(
-                    children: [
-                      Expanded(child: Container(color: Colors.lightBlue)),
+//                 SizedBox(
+//                   height: 100,
+//                   child: Row(
+//                     children: [
+//                       Expanded(child: Container(color: Colors.lightBlue)),
 
-                      SizedBox(width: 20),
+//                       SizedBox(width: 20),
 
-                      Expanded(child: Container(color: Colors.lightBlue)),
+//                       Expanded(child: Container(color: Colors.lightBlue)),
 
-                      SizedBox(width: 20),
+//                       SizedBox(width: 20),
 
-                      Expanded(child: Container(color: Colors.lightBlue)),
-                    ],
-                  ),
-                ),
+//                       Expanded(child: Container(color: Colors.lightBlue)),
+//                     ],
+//                   ),
+//                 ),
 
-                SizedBox(height: 20),
+//                 SizedBox(height: 20),
 
-                Expanded(child: Container(color: Colors.pinkAccent)),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
+//                 Expanded(child: Container(color: Colors.pinkAccent)),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
