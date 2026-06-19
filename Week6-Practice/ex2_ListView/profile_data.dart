26121bef3da325bdcc89efe8603361dc7bf79130
Reakson <v.monireak15@gmail.com>
2026-06-19 08:01:41 +0700

    Change file structures

---
+++ Week6-Practice/ex2_ListView/profile_data.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+
+// import '../../model/profile_tile_model.dart';
+
+// ProfileData monireakProfile = ProfileData(
+//   name: "Monireak",
+//   position: "Cybersecurity Student",
+//   avatarUrl: 'assets/avatars/chetra.jpg',
+//   tiles: [
+//     TileData(icon: Icons.phone, title: "Phone Number", value: "+123 456 7890"),
+//     TileData(icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
+//     TileData(
+//       icon: Icons.email,
+//       title: "Mail",
+//       value: "vanmonireak.oeun@student.cadt.edu.kh",
+//     ),
+//   ],
+// );
---
+++ Week6-Practice/ex3/main.dart
@@ -0,0 +1,45 @@
+// import 'package:flutter/material.dart';
+// import 'ui/screens/temperature_screen.dart';
+// import 'ui/screens/welcome_screen.dart';
+
+// class TemperatureApp extends StatefulWidget {
+//   const TemperatureApp({super.key});
+
+//   @override
+//   State<TemperatureApp> createState() {
+//     return _TemperatureAppState();
+//   }
+// }
+
+// class _TemperatureAppState extends State<TemperatureApp> {
+//   bool _showConverter = false;
+
+//   void _onStart() {
+//     setState(() => _showConverter = true);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//               colors: [Color(0xff16C062), Color(0xff00BCDC)],
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//             ),
+//           ),
+//           child:
+//               _showConverter // 👈 conditional here
+//               ? TemperatureScreen()
+//               : WelcomeScreen(onStart: _onStart),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(const TemperatureApp());
+// }
